005c6aee3d3961a7eeed33635274458f411629cf
Abhinab Sharma <146541054+Abhinab04@users.noreply.github.com>
2025-02-01 20:24:27 +0530

    fixed

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Login from './Components/Login/Login'
+import './App.css'
+
+function App() {
+   return (
+      <div>
+         <Login />
+      </div>
+   )
+}
+
+export default App
