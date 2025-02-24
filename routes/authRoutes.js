8fe24e1eb3531d031f812c78d4475cb0ec52f05e
Soap-Mac <arpitmishra5096@gmail.com>
2025-02-24 14:45:21 +0530

    auth updated

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,7 +12,7 @@ router.post('/signup', async (req, res) => {
         console.log('try k andar');
         if (!userName || !email || !password) {
             console.log('not passward wala check');
-            return res.status(500).send({
+            return res.status(500).json({
                 sucess: false,
                 message: 'please fill all the feilds ',
             })
@@ -21,9 +21,9 @@ router.post('/signup', async (req, res) => {
         const exist = await schema.findOne({ email: email })
         if (exist) {
             console.log('exist wala check');
-            return res.status(500).send({ // render the register pge 
+            return res.status(500).json({ // render the register page with message ki user already exist krta h 
                 sucess: true,
-                message: 'user already exit try to sinup',
+                message: 'user already exits try to sinup',
             })
         }
         console.log('salt se pahele ka line');
@@ -41,29 +41,28 @@ router.post('/signup', async (req, res) => {
             password: newPassword,
         })
         req.session.userId = newuser._id;
-        res.status(201).send({
+        res.status(201).json({  // yaha render krna h register k baad ka page user create ho chuka h
             sucess: true,
             message: 'new User Created'
         })
     } catch (error) {
         console.log(error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).json('Internal Server Error');
     }
 })
 
 router.post('/login', async (req, res) => {
-    // res.send('login k andar');
     try {
         const { userName, email, password } = req.body;
         if (!userName || !email || !password) {
-            return res.status(500).send({
+            return res.status(500).json({
                 sucess: false,
                 message: 'form pura bharo'
             })
         }
         const exist = await schema.findOne({ email: email });
         if (!exist) {
-            return res.status(500).send({ // yaha wapas login render krwa dena h with msg ki email not registered
+            return res.status(500).json({ // yaha wapas login render krwa dena h with msg ki email not registered
                 sucess: false,
                 message: 'user not found'
             })
@@ -72,13 +71,13 @@ router.post('/login', async (req, res) => {
         const same = await bcrypt.compare(password, exist.password);
         console.log('same wala line run ho gaya h');
         if (!same) {
-            return res.status(500).send({ // yaha wapas login render krwa dena h with msg ki password incorrect
+            return res.status(500).json({ // yaha wapas login render krwa dena h with msg ki password incorrect
                 sucess: false,
                 message: 'password incorrect',
             })
         }
         req.session.userId = exist._id;
-        res.status(200).send({ // yaha render krwayenge login k baad 
+        res.status(200).json({ // yaha render krwayenge login k baad 
             sucess: true,
             message: 'user found'
         })
