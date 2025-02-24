19998200db8f3b345bc1d5a14d30f09183c5df82
Soap-Mac <arpitmishra5096@gmail.com>
2025-02-24 22:46:41 +0530

    connection updated

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,16 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/project2').then(() => {
-    console.log('Connection Eshtablished');
 
+require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connection Eshtablished');
 }).catch((e) => {
+    console.log(process.env.MONGODB_URI)
     console.log('Connection Not Eshtablished');
     console.log(e);
 
 
-})
+});
 
+// module.exports = {
+//     MongoURI: process.env.MONGODB_URI || "mongodb://localhost:27017"
+// }
