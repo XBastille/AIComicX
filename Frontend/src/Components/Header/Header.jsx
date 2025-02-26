a8932e561183c2fc033f9817e8ea0f783a0f70ba
Ayush10761op <163870658+Ayush10761op@users.noreply.github.com>
2025-02-26 22:42:42 +0530

    Add files via upload

---
+++ Frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Header.css";
+import githubIcon from "../../Picture/github.png";
+import linkedinIcon from "../../Picture/linkedin.png";
+import instagramIcon from "../../Picture/instagram-new.png";
+import twitterIcon from "../../Picture/X_logo_2023_white.png";
+
+
+function Header () {
+   /* const [isOpen, setIsOpen] = useState(true);*/
+    return (
+            <div className="menu-overlay">
+              <div className="menu-content">
+                <button className="close-button">
+                   CLOSE
+                </button>
+                <div className="menu-container">
+                <ul className="menu-list">
+                  {[
+                    "Home",
+                    "Team",
+                    "FAQs",
+                    "Socials",
+                    "Donate",
+                    "Contact us",
+                  ].map((item, index) => (
+                    <li key={index}>
+                      <span>{String(index + 1).padStart(2, "0")}</span> {item}
+                    </li>
+                  ))}
+                </ul>
+               <div className="social-icons">
+               {[githubIcon, twitterIcon, instagramIcon, linkedinIcon].map((icon, index) => (
+                <div key={index} className="social-icon-container">
+                  <img src={icon} alt="Social Icon" className="social-icon" />
+                </div>
+              ))}
+                </div>
+                </div> 
+              </div>
+            </div>
+          
+    );
+};
+
+export default  Header;
---
+++ Frontend/src/Components/Nav/Nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Nav.css"
+
+
+function Nav () {
+    return(
+      <nav className="Navbar">
+       <button className="play-btn">LOGIN</button>
+
+       <div className="menu">
+         <span className="menu-text">MENU</span>
+       </div>
+      </nav >
+    )
+}
+
+export default Nav;
