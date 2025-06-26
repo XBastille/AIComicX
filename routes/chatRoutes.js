aae41daa3f01a81469a4b0f4334ec00ab42a9aa8
Arpit Mishra <146555776+Soap-mac@users.noreply.github.com>
2025-06-26 12:50:31 +0530

    sam route for st2nar

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -245,56 +245,35 @@ router.post('/transferSam', async (req, res) => {
     });
 });
 
-router.post('/ayush', upload.single("file"), async (req, res) => {
-    const error = []
-    if (!req.file) {
-        error.push({ msg: "Nothing has been uploaded" })
-    }
-
-    const pythonst2nar = path.join(__dirname, "../pythonInput/st2nar.txt");
-    const filePath = req.file.path;
-    // const filePath = path.join(__dirname, `../uploads/userFile.txt`);
-
-    const ext = path.extname(req.file.originalname).toLowerCase();
-    // const ext = '.txt';
-
-    try {
-
-        if (ext === '.txt') {
-            console.log("inside txt");
-            const data = fs.readFileSync(filePath, "utf-8");
-            fs.writeFileSync(pythonst2nar, data);
-        }
-        else {
-            error.push({ msg: "Unsupported file type" });
-            return res.json({ success: false, msg: "Only PDF , TXT and DOCX files allowed", error });
-        }
-
-    } catch (error) {
-        console.log(error);
-    }
-
-    if (error.length > 0) {
-        return res.json({ sucess: false, msg: "re render the choose page", error })
-    }
+router.post('/ayush', async (req, res) => {
     const scriptPath = path.join(__dirname, '../genai_models/st2nar.py');
-    try {
-        console.log("pythonConect se pahle");
-        console.log("pythonst2nar", pythonst2nar);
-        console.log("scriptPath", scriptPath);
-        const response = await pythonConnect(res, pythonst2nar, scriptPath);
-        console.log("pythonConnect ke baad");
-        console.log("Response of the python script is ", response);
-        if (response) {
-            console.log("Response k andar aa gaya");
-            return res.json({ sucess: true, msg: "Data is ready", result: response });
+    const { text } = req.body;
+
+    const pythonProcess = spawn('C:\\Program Files\\Python313\\python.exe', [scriptPath, text]);
+
+    let output = '';
+    let errorOutput = '';
+
+    pythonProcess.stdout.on('data', (data) => {
+        output += data.toString();
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+        errorOutput += data.toString();
+    });
+
+    pythonProcess.on('close', (code) => {
+        console.log(`Process exited with code ${code}`);
+
+        if (code !== 0 || errorOutput) {
+            console.error(errorOutput);
+            return res.status(500).json({ error: errorOutput || 'Python script error' });
         }
 
-    } catch (err) {
+        res.json({ result: output });
+    });
+});
 
-        console.error("Python execution failed:", err);
-    }
-})
 
 
 module.exports = router;
