fa999a4a0a4b757c4a07d627da48d9764c7080f4
Abhinab Sharma <146541054+Abhinab04@users.noreply.github.com>
2025-07-08 21:45:58 +0530

    Add files via upload

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,28 +8,11 @@ import Nav from './Components/Nav/Nav'
 import Footer from './Components/Footer/Footer'
 import Conssole from './Pages/Conssole/Conssole'
 import ConssoleNav from './Components/Nav/ConssoleNav'
-import One from './Components/Grid/panel1/One';
 import PromptPage from './Pages/PromptPage/PromptPage'
-import LoadingAnimation from './Components/LoadingAnimation/LoadingAnimation'
-// import LoadingAnimation2 from './Components/LoadingAnimation/LoadingAnimation2'
 
 import './App.css'
 
 function App() {
-   const [isInitialLoading, setIsInitialLoading] = useState(true);
-
-   const handleLoadingComplete = () => {
-      setIsInitialLoading(false);
-   };
-
-   if (isInitialLoading) {
-      return <LoadingAnimation onComplete={handleLoadingComplete} />;
-   }
-
-   // if (isInitialLoading) {
-   //    return <LoadingAnimation2 />;
-   // }
-
    return (
       <Router>
          <Routes>
@@ -39,12 +22,17 @@ function App() {
             <Route path='/user/Register' element={<Register />} />
             <Route path='/user/Login' element={<Login />} />
 
+
             {/*Main Pages routing */}
+
             <Route path="/SelectPage" element={<ChoosePage />} />
             <Route path="/Generate_Story" element={<Conssole />} />
             <Route path="/PromptPage" element={<PromptPage />} />
          </Routes>
       </Router>
+      // <div>
+      //    <Body />
+      // </div>
    )
 }
 
