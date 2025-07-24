c46be5e815f4b2c25ec8c8018c68fcdafe63bd85
Arpit Mishra <146555776+Soap-mac@users.noreply.github.com>
2025-07-24 14:11:33 +0530

    loading animation enhanced and fixed

--- Frontend/src/Components/LoadingAnimation/Simple.jsx
+++ Frontend/src/Components/LoadingAnimation/Simple.jsx
@@ -6,6 +6,10 @@ const LoadingSpinner = () => {
         <div className="loading-overlay">
             <div className="loading-containers">
                 <div className="loading-spinner"></div>
+                <p>Generating</p>
+                <span className="loading-text-animation-dot1">.</span>
+                <span className="loading-text-animation-dot2">.</span>
+                <span className="loading-text-animation-dot3">.</span>
             </div>
         </div>
     )
